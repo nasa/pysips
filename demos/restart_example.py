e025219d35fcd14817547605e7b75c2c3775a1a2
Patrick Leser <patrick.e.leser@gmail.com>
2025-09-09 17:09:04 -0400

    working version of the restart example w/ pickle storage

--- demos/restart_example.py
+++ demos/restart_example.py
@@ -1,10 +1,9 @@
-import threading
-import time
-
+import multiprocessing
 import numpy as np
+
+from pathlib import Path
 from pysips import PysipsRegressor
 from sklearn.metrics import r2_score
-import matplotlib.pyplot as plt
 
 
 def init_x_vals(start, stop, num_points):
@@ -33,9 +32,11 @@ def run_pysips(max_time=None):
         # effectiveness also increases (but may saturate at larger values)
         num_mcmc_samples=10,
         # to control randomness
-        # random_state=42,
+        random_state=42,
         # setting a time limit
         max_time=max_time,
+        # set a checkpoint file to save progress in case of interruption
+        checkpoint_file="example.chkpt",
     )
 
     regressor.fit(X, y)
@@ -49,26 +50,29 @@ def run_pysips(max_time=None):
     return regressor.phis_, regressor.likelihoods_
 
 
-def force_timeout(signum, frame):
-    raise TimeoutError("Function timed out!")
-
-
 def main():
-    timeout_seconds = 10
-    thread = threading.Thread(target=run_pysips)
-    thread.daemon = True  # Dies when main thread dies
-    thread.start()
-    thread.join(timeout_seconds)
 
-    if thread.is_alive():
-        print(f"Function stopped after {timeout_seconds} seconds")
+    # remove checkpoint file if exists
+    (Path(__file__).parent / "example.chkpt").unlink(missing_ok=True)
+
+    timeout_seconds = 20
+    process = multiprocessing.Process(target=run_pysips)
+    process.start()
+    process.join(timeout_seconds)
+
+    if process.is_alive():
+        print(f">> Killing run_pysips after {timeout_seconds} seconds.")
+        process.terminate()
+        process.join()
     else:
-        print("Function completed naturally")
+        print(">> Function completed naturally.")
 
     # restart and complete
+    print(">> Restarting run_pysips and waiting for completion...")
     run_pysips()
 
-    # what if I run this and it was already completed?
+    # note that additional calls will not run
+    print(">> Restarting run_pysips again does nothing:")
     run_pysips()
 
 
